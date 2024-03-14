16827e5877e6cb3154f1d7790a229df82c0e69d1
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-14 20:04:19 -0300

    Constructors

--- OOP/OOP.java
+++ OOP/OOP.java
@@ -1,10 +1,13 @@
 public class OOP {
     public static void main(String[] args) {
-        User u = new User();
-        u.set_name("Caleb");
-        u.set_membership(User.Membership.Gold);
+        User u = new User("Caleb", "Gold");
+        User u2 = new User();
 
         System.out.println(u.get_name());
         System.out.println(u.get_membership());
+
+        
+        System.out.println(u2.get_name()); // null
+        System.out.println(u2.get_membership()); // Bronze
     }
 }
--- OOP/User.java
+++ OOP/User.java
@@ -1,7 +1,16 @@
 class User {
 
     private String name;
-    private String membership;
+    private String membership = "Bronze";
+
+    public User() {
+        
+    }
+
+    public User(String name, String membership) {
+        set_name(name);
+        set_membership(membership);
+    }
 
     // Setter
     void set_name(String name) {
