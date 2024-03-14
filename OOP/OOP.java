4c730f50605719ba2fa398b9eb6900479050ed1b
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-14 19:25:47 -0300

    Getters and Setters

--- OOP/OOP.java
+++ OOP/OOP.java
@@ -1,15 +1,9 @@
-
 public class OOP {
     public static void main(String[] args) {
         User u = new User();
-        u.name = "Caleb";
+        u.set_name("Caleb");
         u.membership = "Gold";
 
-        User u2 =  new User();
-        u2.name = "Sally";
-        u2.membership = "Silver";
-
-        System.out.println(u.name);
-        System.out.println(u2.name);
+        System.out.println(u.get_name());
     }
 }
--- OOP/User.java
+++ OOP/User.java
@@ -1,8 +1,15 @@
-
-public class User {
-    
-    String name;
+class User {
+    private String name;
     String membership;
 
-    
+    // Setter
+    void set_name(String name) {
+        System.out.println("Logging to log file...");
+        this.name = name;
+    }
+
+    // Getter
+    String get_name() {
+        return name;
+    }
 }
